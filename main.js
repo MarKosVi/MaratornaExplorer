059e94497245eac768690e5fffbdf183911d0231
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-06-11 01:05:06 -0300

    func Rsp

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let body = document.querySelector('body');
 let section = document.createElement('section');
 let h1 = document.createElement('h1');
 let p = document.createElement('p');
+let result = document.createElement('p');
 let input = document.createElement('input');
 let buttons = document.createElement('button');
 let img = document.createElement('img')
@@ -13,6 +14,7 @@ img.src = './assets/bola_cristal.png';
 input.setAttribute('type', 'Text');
 input.setAttribute('placeholder', ' Digite sua pergunta');
 input.setAttribute('id', 'input');
+result.setAttribute('class', 'disabled');
 buttons.setAttribute('id', 'button');
 img.setAttribute('alt', 'Imagem de uma bola de cristal');
 
@@ -29,14 +31,47 @@ section.appendChild(p);
 section.appendChild(input);
 section.appendChild(buttons);
 body.appendChild(section);
+section.appendChild(result); 
 
 let button = document.querySelector('#button');
 let campo = document.querySelector('#input');
 
+let Respostas = [ "Certeza!",
+"Não tenho tanta certeza.",
+"É decididamente assim.",
+"Não conte com isso.",
+"Sem dúvidas!",
+"Pergunte novamente mais tarde.",
+"Sim, definitivamente!",
+"Minha resposta é não.",
+"Você pode contar com isso.",
+"Melhor não te dizer agora.",
+"A meu ver, sim.",
+"Minhas fontes dizem não.",
+"Provavelmente.",
+"Não é possível prever agora.",
+"Perspectiva boa.",
+"As perspectivas não são tão boas.",
+"Sim.",
+"Concentre-se e pergunte novamente.",
+"Sinais apontam que sim."];
+
 // function
+ // Limpar input
+function clear() {
+    setTimeout(()=> {
+        result.removeAttribute('ativate');
+        result.setAttribute('class', 'disabled');
+    } ,4000);
+}
 
 button.addEventListener('click', () => {
     if(campo.value.length != ' '){
-        alert(campo.value);
+        let num = Math.floor(Math.random()* Respostas.length);
+        result.innerText = Respostas[num];
+        result.removeAttribute('disabled');
+        result.setAttribute('class', 'ativate');
+        clear();
+
     }
 })
